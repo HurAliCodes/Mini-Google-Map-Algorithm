5d7be894907f23a203f8c4cf1a4c16c666edc03a
Abdul-Ahad-0814 <abdulahadmemon567@gmail.com>
2025-10-15 09:07:26 +0500

    parsing  of osm data done

--- include/parsing.h
+++ include/parsing.h
@@ -1,1 +1,10 @@
 #include"Graph.h"
+#include<pugixml.hpp>
+#include<iostream>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
+void parseOSM(Graph& graph, const string& filename);
+void exporttocsv(Graph& graph1);
