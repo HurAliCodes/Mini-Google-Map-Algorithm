91a5aa2f8a86ce60b40fabe14d301fb8e12e13d2
Hur_Ali <huralirizvi042@gmail.com>
2025-10-19 14:38:38 +0500

    API Server Created

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,33 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include"pugixml.hpp"
-#include"Graph.h"
-#include"parsing.h"
-#include"Algo.h"
-using namespace std;
+#include "crow.h"  // or "crow.h" depending on your version
 
+int main()
+{
+    crow::SimpleApp app;
 
-int main() {
-    Algorithms a;
-    Graph g;
-    parseOSM(g, "Karachi/Karachi.osm");
-    // exporttocsv(g);
-    a.Dijkstra(g,13167991882, 13167991885);
-    return 0;
+    // --- GET route ---
+    CROW_ROUTE(app, "/")([](){
+        return "Server is running ✅";
+    });
+
+    // --- GET with parameter ---
+    CROW_ROUTE(app, "/hello/<string>")
+    ([](std::string name){
+        return "Hello, " + name + " 👋";
+    });
+
+    // --- POST route ---
+    CROW_ROUTE(app, "/data").methods("POST"_method)
+    ([](const crow::request& req){
+        crow::json::rvalue body = crow::json::load(req.body);
+        if (!body)
+            return crow::response(400, "Invalid JSON");
+
+        std::string message = body["message"].s();
+        crow::json::wvalue res;
+        res["reply"] = "You sent: " + message;
+        return crow::response(res);
+    });
+
+    // --- Start the server ---
+    app.port(18080).multithreaded().run();
 }
--- README.md
+++ README.md
@@ -2,6 +2,9 @@ Download Pugixml headers Zipped file
 https://drive.google.com/file/d/1-Bi5p-1cugX-sQzv-trhg1gRrftMdizA/view?usp=sharing
 g++ -Iinclude src/*.cpp Main.cpp -o main.exe
 
+### command to run server
+g++ -std=c++17 -Iinclude main.cpp -o server -pthread -lws2_32 -lmswsock
+
 .Gitignore (add this)
 # Ignore large map and archive files
 Karachi OSM/
